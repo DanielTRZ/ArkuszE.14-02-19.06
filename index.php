c086aa8fada09f1f0459a602a28ed0222afc3cbf
DanielTRZ <71105883+DanielTRZ@users.noreply.github.com>
2022-02-15 00:26:14 +0100

    Update index.php

--- index.php
+++ index.php
@@ -23,9 +23,9 @@
 			while($data = mysqli_fetch_assoc($q1))
 			{
 				echo '<tr>
-						<td>'.$data['nazwa'].'</td>
+	                   			<td>'.$data['nazwa'].'</td>
 						<td>'.$data['cena'].' '.'zł'.'</td>
-					</tr>';
+				     </tr>';
 			}
 			
 		echo '</table>';
