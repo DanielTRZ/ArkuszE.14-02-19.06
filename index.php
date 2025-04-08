0cf68ba4ef6730960b73c6e4100bd82a368af68d
Daniel Trzeciński <71105883+DanielTRZ@users.noreply.github.com>
2025-04-08 14:23:50 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset='utf-8'>
 <title>Hurtownia Papiernicza</title>
-	<link rel='Stylesheet' href='styl.css'>
+<link rel='Stylesheet' href='styl.css'>
 </head>
 <body>
 	<div id='baner'>
