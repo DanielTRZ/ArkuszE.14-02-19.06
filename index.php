4289986ab037fb6073c3a36c043cf08f77c3ea78
Daniel Trzeciński <71105883+DanielTRZ@users.noreply.github.com>
2025-04-15 23:08:25 +0200

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ $connect=mysqli_connect('localhost','root','','bazasklep')or die ("Błąd połą
 $q1=mysqli_query($connect,'SELECT nazwa, cena FROM towary WHERE id <= 4'); 
 		
 echo'<table>
-	<tr>
+<tr>
 			</tr>';		
 			while($data = mysqli_fetch_assoc($q1))
 			{
