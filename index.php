abdc031c4702b9e6b9a7359b4eccac2097396a22
Daniel Trzeciński <71105883+DanielTRZ@users.noreply.github.com>
2025-04-17 05:44:34 +0200

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ $q1=mysqli_query($connect,'SELECT nazwa, cena FROM towary WHERE id <= 4');
 		
 echo'<table>
 <tr>
-		</tr>';		
+	</tr>';		
 			while($data = mysqli_fetch_assoc($q1))
 			{
 				echo '<tr>
