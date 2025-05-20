1bf53a25379bd5f3a23b13b7fdcc75de37935e62
Daniel Trzeciński <71105883+DanielTRZ@users.noreply.github.com>
2025-05-20 19:49:55 +0200

    Update index.php

--- index.php
+++ index.php
@@ -33,7 +33,7 @@ echo '</table>';
 ?>	
 </div>
 	
-	<div id='srodek'>
+<div id='srodek'>
 		<h3>Ile będą kosztować Twoje zakupy?</h3>
 		<form method='post' action='index.php'>
 			wybierz artykuł <select name='wybart'>
