424dbd56c9c26cd5d896aa4c5c48b43a91f12009
Daniel Trzeciński <71105883+DanielTRZ@users.noreply.github.com>
2025-07-05 12:40:16 +0200

    Update index.php

--- index.php
+++ index.php
@@ -44,7 +44,7 @@ wybierz artykuł <select name='wybart'>
 <option value='Ekierka'>Ekierka</option>
 <option value='Linijka 50 cm'>Linijka 50cm</option>
 </select><br>
-		liczba sztuk: <input type='number' value='1' name='sztuk'><br>
+	liczba sztuk: <input type='number' value='1' name='sztuk'><br>
 			<input type='submit' value='OBLICZ'>
 		</form>
 	
